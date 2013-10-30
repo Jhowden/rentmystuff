b60d0fa15deb528d886635d8c9deb878adcc4829
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 14:38:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuffwithfriends::Application.config.secret_token = 'e7cb8d372e04ddbe1d1ad234891c15b757f287861e51c9d1a1856343566dc1f6039d33ea38b30d8b6edbc783692121897c283be84b1757049663951e12a8c82d'
