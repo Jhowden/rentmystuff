85b543e933ee92a1ab3f279a132252b9f1f6df30
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 18:46:39 -0500

    added nested routes between users and items to reflect their association

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Stuffwithfriends::Application.routes.draw do
-  resources :items
+  
+  resources :users, :only => [:show] do
+    resources :items, :only => [:new, :edit, :create, :update, :delete]
+  end
+
+  resources :items, :only => [:index, :show]
+
   get '/logout', :to => "sessions#destroy"
   get '/auth/:provider/callback', :to => "sessions#create"
   get '/auth/failure', redirect('/')
