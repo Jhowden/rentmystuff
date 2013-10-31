2bb1a6e6a8dc306fce5a2c19cc943d62e4441fe1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 21:37:18 -0500

    Started to add parallax scrolling to css

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Stuffwithfriends::Application.routes.draw do
   get '/logout', :to => "sessions#destroy"
   get '/auth/:provider/callback', :to => "sessions#create"
   get '/auth/failure', redirect('/')
+
+  root :to => 'pages#index'
 end
