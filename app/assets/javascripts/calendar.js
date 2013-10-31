1e368057d75af31c68bec36711eaaf2960f94278
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 15:17:36 -0500

    refactored calendat javascript

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-  var currentTime = new Date();
   var disabled = ['2013-11-04', '2013-10-28', '2013-10-27'];
+  var maxDate = 60
 
 $('#from-date-picker').datepicker({
   constrainInput: true,
   minDate: 0,
-  maxDate: 30,
+  maxDate: maxDate,
   beforeShowDay: function(date){
     var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
     return [ disabled.indexOf(string) == -1 ];
@@ -16,7 +16,7 @@ $('#from-date-picker').datepicker({
 $('#to-date-picker').datepicker({
   constrainInput: true,
   minDate: 0,
-  maxDate: 30,
+  maxDate: maxDate,
   beforeShowDay: function(date){
     var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
     return [ disabled.indexOf(string) == -1 ];
