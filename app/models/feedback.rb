0f19e06d223add7aeccf6e05f66da9e27e6fe218
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-30 21:29:48 -0500

    changed feedback association with borrowings

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,8 +4,8 @@ class Feedback < ActiveRecord::Base
   validates :thumbs_up, :inclusion => {:in => [true,false]}
   validates_presence_of :comment
 
-  has_many :borrowings
-  has_many :items, :through => :borrowings
+  has_one :borrowing
+  has_one :item, :through => :borrowings
 
   belongs_to :giver, :class_name => 'User'
 end
