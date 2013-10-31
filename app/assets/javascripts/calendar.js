b937a22446b902f9e403a325f2d304365d0d16e7
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-31 15:14:40 -0500

    added date restrictions to calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
-  var today = new Date();
-  var disabled = ['2013-10-31', '2013-10-28', '2013-10-27'];
+  var currentTime = new Date();
+  var disabled = ['2013-11-04', '2013-10-28', '2013-10-27'];
 
-  $('#from-date-picker').datepicker({
-    constrainInput: true,
-    beforeShowDay: function(date){
-        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
-        return [ disabled.indexOf(string) == -1 ]
-      };
-  });
+$('#from-date-picker').datepicker({
+  constrainInput: true,
+  minDate: 0,
+  maxDate: 30,
+  beforeShowDay: function(date){
+    var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+    return [ disabled.indexOf(string) == -1 ];
+  }
+});
 
-  $('#to-date-picker').datepicker({
-    constrainInput: true,
-    beforeShowDay: function(date){
-        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
-        return [ disabled.indexOf(string) == -1 ]
-      };
-  });
-})
+$('#to-date-picker').datepicker({
+  constrainInput: true,
+  minDate: 0,
+  maxDate: 30,
+  beforeShowDay: function(date){
+    var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+    return [ disabled.indexOf(string) == -1 ];
+  }
+});
+});
