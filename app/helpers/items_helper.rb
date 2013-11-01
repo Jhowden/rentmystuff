9aceccceae88e5c15c217ea825db9cbbbe61f3fd
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-01 02:16:58 -0500

    added datepicker restrictions, updates to search but not fully functional

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
    
    def parse_search_dates!(params)
 
-    params[:q][:available_dates_cont] = parse_dates(params).to_yaml
+    params[:q][:available_dates_cont] = parse_dates(params).to_yaml[3...-1]
    
    end
 
@@ -13,10 +13,10 @@ module ItemsHelper
   private
 
   def parse_dates(params)
-    start_date = Date.strptime(params[:start_date], '%m/%d/%y')
-    end_date = Date.strptime(params[:end_date], '%m/%d/%y')
+    start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
+    end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
 
-    (start_date..end_date).to_a
+    (start_date...end_date).to_a
   end
 
 end
