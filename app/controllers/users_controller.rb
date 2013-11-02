70635404a81991d5a56d14f5b934082d4cdc853f
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 16:20:34 -0500

    added ability for user to change their 'about me' information and implemented ajax on the form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def show
-    
+
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+
+
+    if request.xhr?
+      render :json =>  {"about" => @user.about}
+    else
+      redirect_to user_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :first_name, :last_name, :email, :image_url
+  attr_accessible :first_name, :last_name, :email, :image_url, :about
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
