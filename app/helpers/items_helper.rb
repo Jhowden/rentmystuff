25f26d1d522428c22f3890a0cb623dd25f65d232
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-02 19:00:52 -0500

    added styling to items index, added seeds

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -16,8 +16,18 @@ module ItemsHelper
   private
 
   def parse_dates(params)
-    start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
-    end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
+    if params[:start_date].match(/dd\/dd\/dddd/)
+      start_date = Date.strptime(params[:start_date], '%m/%d/%Y')
+    else
+      start_date = Date.today
+    end
+
+
+    if params[:end_date].match(/dd\/dd\/dddd/)
+      end_date = Date.strptime(params[:end_date], '%m/%d/%Y')
+    else
+      end_date = 30.days.from_now.to_date
+    end
 
     (start_date...end_date)
   end
