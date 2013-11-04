845e778f975274e67fdbed98708afa13c37f8a7d
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-03 18:34:09 -0600

    added conditional for feedbacks, added front page links, some styling

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,16 +1,29 @@
 $(document).ready(function() {
-   $(document).on('submit',".feedback_form", function(event) {
-    event.preventDefault();
-    var data = $(this).serialize();
-    var that = $(this)
-    $.post(window.location.pathname+"/feedbacks/create", data, function(response) {
-      that.after(response);
-      that.remove();
-    });
-  });
+  //  $(document).on('submit',"#feedback_form", function(event) {
+  //   event.preventDefault();
+  //   var data = $(this).serialize();
+  //   var that = $(this)
+  //   $.post(window.location.pathname+"/feedbacks/create", data, function(response) {
+  //     that.after(response);
+  //     that.remove();
+  //   });
+  // });
+
+  //  $(document).on('submit',"#first_feedback_form", function(event) {
+  //   event.preventDefault();
+  //   var data = $(this).serialize();
+  //   var that = $(this)
+  //   $.post(window.location.pathname+"/feedbacks/create", data, function(response) {
+  //     that.after(response);
+  //     that.remove();
+  //   });
+  // });
 
    $(document).on("click", "#show-feedback", function(){
-    console.log($(".feedback_form"));
-    $(".feedback_form").slideToggle();
+    $("#feedback_form").slideToggle();
+   })
+
+   $(document).on("click", "#first-show-feedback", function(){
+    $("#first_feedback_form").slideToggle();
    })
 });
